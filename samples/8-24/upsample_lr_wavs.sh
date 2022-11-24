1366b5f14598fba091e40a2e536abe80d92fe235
mosem <mandelmoshe@gmail.com>
2022-11-24 14:04:54 +0200

    removed /samples,/static and index.html

--- samples/8-24/upsample_lr_wavs.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-find **/lr.wav -type f -exec bash -O nullglob -c 'sox "$0" -r 24000 "$(dirname $0)/lr_up.wav"' {} \;
-
--- samples/adv_ablation/upsample_lr_wavs.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-find **/lr.wav -type f -exec bash -O nullglob -c 'sox "$0" -r 16000 "$(dirname $0)/lr_up.wav"' {} \
